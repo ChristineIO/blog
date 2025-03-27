949b1ac8fa27c7f831f9d8a12da087c74a0f6b5a
ChristineIO <lifeanddigit@gmail.com>
2025-03-27 22:31:17 +0100

    starting to work on the posts page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,15 +9,16 @@ const Home = () => {
                 <div className='logo'>
                     <h1><Link to='/' className='logo'>Blog</Link></h1>
                 </div>
+                    <Link to='/blogs' className='auth-btn post-btn'>Posts</Link>
 
                 <div className='auth-buttons'>
-                    <Link to='/signup'>Sign up</Link>
-                    <Link to='/login'>Login</Link>
+                    <Link to='/signup' className='auth-btn'>Sign up</Link>
+                    <Link to='/login' className='auth-btn'>Login</Link>
                 </div>
             </div>
             <div className='posts'>
-                     <Post />
-                </div>
+                <Post />
+            </div>
         </div>
     )
 }
